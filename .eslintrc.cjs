ab02c4889c48ed6f4a2da887fa2601beac371aa6
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-12-21 18:54:58 +0900

    Vite + Babel + TypeScript + ESLint 설정 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  rules: {
+    'max-len': ['error', { code: 150 }],
+    'import/extensions': ['error', {
+      ts: 'never',
+      tsx: 'never',
+      json: 'always',
+      scss: 'always',
+    }],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint', 'react'],
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+      ],
+      rules: {
+        'max-len': ['error', { code: 150 }],
+        'import/extensions': ['error', {
+          ts: 'never',
+          tsx: 'never',
+          json: 'always',
+          scss: 'always',
+        }],
+      },
+    },
+  ],
+};
